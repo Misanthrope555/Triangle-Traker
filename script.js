c5ffb7f2c67b23a9d57a0eaaccee8716dd1d14d2
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-28 23:44:22 +0300

    deleted every code and started afresh

--- script.js
+++
@@ -1,9 +0,0 @@
-function triangle(){
-var sideA = parseInt(document.sides.sideA.value);
-var sideB = parseInt(document.sides.sideB.value);
-var sideC = parseInt(document.sides.sideC.value);
-
-if(sideA< (sideA+sideB)&&(sideC < sideA+sideB)(sideB<sideA+sideC)){
-    window.alert("This can form a triangle");
-}
-}
