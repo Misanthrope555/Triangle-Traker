07a317a84cb29b06f6cae2ff84ec4bce0d5c90ea
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-29 00:03:05 +0300

    created a function and decleared variables

---
+++ script.js
@@ -0,0 +1,7 @@
+function Triangle(){
+    var base = parseFloat(prompt("Enter the base length of the triangle"));
+    var height = parseFloat(prompt("Enter the height lenght of the triangle"));
+    var hypotenus = parseFloat(prompt("Enter the hypotenus length of the triangle"));
+
+   
+}
