4cfc4d89bf98e13dedf972376fdc6ef87271198e
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-28 14:43:12 +0300

    decleared variables of different sides of the triangle

---
+++ script.js
@@ -0,0 +1,6 @@
+function triangle(){
+    //convert input to integers
+    var sideA = (document.side.sideA.value);
+    var sideB = (document.side.sideB.value);
+    var sideC = (document.side.sideC.value);
+}
