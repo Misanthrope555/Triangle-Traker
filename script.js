fa035a1d3bcb596136cc3dda06f5ffb3a1f4004c
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-28 18:30:18 +0300

    declared variables to be used

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 function triangle(){
-    //convert input to integers
-    var sideA = parseInt(document.side.sideA.value);
-    var sideB = parseInt(document.side.sideB.value);
-    var sideC = parseInt(document.side.sideC.value);
+var sideA = parseInt(document.sides.sideA.value);
+var sideB = parseInt(document.sides.sideB.value);
+var sideC = parseInt(document.sides.sideC.value);
 
-    //squares the sides of the triangle
-    var sideAsquared = parseInt(document.side.sideA.value * document.side.sideA.value);
-    var sideBsquared = parseInt(document.side.sideB.value * document.side.sideB.value);
-    var sideCsquared = parseInt(document.side.sideC.value * document.side.sideC.value );
-
-    //checks two sides of a triangle to determine if a triangle can be formed
-    var sideAB = parseInt(document.side.sideA.value) + (document.side.sideB.value);
-    var sideAC = parseInt(document.side.sideA.value) + (document.side.sideC.value);
-    var sideBC = parseInt(document.side.sideC.value) + (document.side.sideB.value);
-
-    if((sideAB>sideC)&& (sideAC>sideB)&& (sideBC>sideA)){
-        window.alert("These will form a triangle");
-
-    }
-
-  
-
+if(sideA< (sideA+sideB)&&(sideC < sideA+sideB)(sideB<sideA+sideC)){
+    window.alert("This can form a triangle");
+}
 }
