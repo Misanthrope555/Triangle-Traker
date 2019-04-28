76aa688ce2347f61f565d85effb7c4ee035c3d03
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-29 01:03:10 +0300

    changed color to css

--- script.js
+++ script.js
@@ -4,16 +4,35 @@ function Triangle(){
     var hypotenus = parseFloat(prompt("Enter the hypotenus length of the triangle"));
 
     if(base===height && height===hypotenus && hypotenus === base){
-        alert("This is an equilateral triangle");
+        alert("NICE!! based on the the infomation you have given us, that should form an equilateral triangle");
+           
     }
     else if(base===height && height === hypotenus || hypotenus===base){
-        alert("An isosceles triangle");
+        alert("NICE!! based on the the infomation you have given us,that should form an  isosceles triangle");
     }
     else if((base + height) <= hypotenus||(height+hypotenus)<=base||(hypotenus+base)<= height){
-        alert("This is not a triangle");
+        alert("Unfortunately,that will not a triangle");
 
     }
     else if(base <= height <= hypotenus){
-        alert("This is a scalene triangle")
+        alert("NICE!! based on the the infomation you have given us,that should forma scalene triangle")
     }
 }
+
+function createTriangle(x1,y1,x2,y2,x3,y3,){
+    var trianglee = new Object();
+    trianglee.x1 = x1;
+    trianglee.x2 = x2;
+    trianglee.x3 = x3;
+    trianglee.y1 = y1;
+    trianglee.y2 = y2;
+    trianglee.y3 = y3;
+
+    trianglee.draw = function() {
+        draw.trianglee(this.x1, this.y1,this.x2,this.y2,this.x3,this.y3);
+    }
+    return trianglee;
+}
+var trianglee = createTriangle(100,100,300,120,280,350);
+        trianglee.draw();
+        
