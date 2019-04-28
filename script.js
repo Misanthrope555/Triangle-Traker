ba4544de506b9789d35faf89a9f23203914e45ef
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-28 15:20:13 +0300

    determine the values of the triangle to be formed

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ function triangle(){
     var sideAsquared = parseInt(document.side.sideA.value * document.side.sideA.value);
     var sideBsquared = parseInt(document.side.sideB.value * document.side.sideB.value);
     var sideCsquared = parseInt(document.side.sideC.value * document.side.sideC.value );
+
+    //checks two sides of a triangle
+    var sideAB = parseInt(document.side.sideA.value) + (document.side.sideB.value);
+    var sideAC = parseInt(document.side.sideA.value) + (document.side.sideC.value);
+    var sideCB = parseInt(document.side.sideC.value) + (document.side.sideB.value);
+    
 }
