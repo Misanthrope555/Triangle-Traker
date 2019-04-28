510249bee051485fd009657e4f76c29557989125
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-29 00:04:02 +0300

    used the if statements

--- script.js
+++ script.js
@@ -3,5 +3,17 @@ function Triangle(){
     var height = parseFloat(prompt("Enter the height lenght of the triangle"));
     var hypotenus = parseFloat(prompt("Enter the hypotenus length of the triangle"));
 
-   
+    if(base===height && height===hypotenus && hypotenus === base){
+        alert("This is an equilateral triangle");
+    }
+    else if(base===height && height === hypotenus || hypotenus===base){
+        alert("An isosceles triangle");
+    }
+    else if((base + height) <= hypotenus||(height+hypotenus)<=base||(hypotenus+base)<= height){
+        alert("This is not a triangle");
+
+    }
+    else if(base <= height <= hypotenus){
+        alert("This is a scalene triangle")
+    }
 }
