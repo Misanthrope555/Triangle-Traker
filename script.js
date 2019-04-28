95b1ce0ddf0afacb4a707f0bd6e1a7eecad80152
uneducated555profesora <uneducated555profesora@localhost.localdomain>
2019-04-28 15:01:12 +0300

    squares the sides of the triangle

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function triangle(){
     //convert input to integers
-    var sideA = (document.side.sideA.value);
-    var sideB = (document.side.sideB.value);
-    var sideC = (document.side.sideC.value);
+    var sideA = parseInt(document.side.sideA.value);
+    var sideB = parseInt(document.side.sideB.value);
+    var sideC = parseInt(document.side.sideC.value);
+
+    //squares the sides of the triangle
+    var sideAsquared = parseInt(document.side.sideA.value * document.side.sideA.value);
+    var sideBsquared = parseInt(document.side.sideB.value * document.side.sideB.value);
+    var sideCsquared = parseInt(document.side.sideC.value * document.side.sideC.value );
 }
